a47e4a6d8e806f9b2407f7fe1812f630e6a8f750
张棋 <385678823@qq.com>
2020-05-02 21:01:40 +0800

    tsconfig调整

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
 import configureStore from './store/configureStore';
+import 'normalize.css';
 import App from './App';
 
 const store = configureStore();
